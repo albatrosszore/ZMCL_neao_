4169e0def30745b1db7c6036e68a53f91bd6eeba
albatrosszore <355188703@qq.com>
2024-03-09 21:24:50 +0800

    添加项目文件。

---
+++ Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using MinecraftLaunch.Classes.Models.Launch;
+using MinecraftLaunch.Components.Authenticator;
+using MinecraftLaunch.Components.Launcher;
+using MinecraftLaunch.Components.Resolver;
+using Wpf.Ui.Controls;
+using ZMCL_neao.ViewModels.Pages;
+using static ZMCL_neao.Views.Pages.SettingsPage;
+
+namespace ZMCL_neao.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var account = new OfflineAuthenticator("Yang114").Authenticate();
+            var resolver = new GameResolver(".minecraft");
+
+            var config = new LaunchConfig
+            {
+                Account = account,
+                IsEnableIndependencyCore = true,
+                JvmConfig = new(javalist_)
+               
+            };
+
+            Launcher launcher = new(resolver, config);
+            var gameProcessWatcher = await launcher.LaunchAsync(gamelist_);
+
+            //获取输出日志
+            gameProcessWatcher.OutputLogReceived += (sender, args) => {
+                Console.WriteLine(args.Text);
+            };
+
+            //检测游戏退出
+            gameProcessWatcher.Exited += (sender, args) => {
+                Console.WriteLine("exit");
+            };
+        }
+    }
+}
---
+++ Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wpf.Ui.Controls;
+using ZMCL_neao.ViewModels.Pages;
+using MinecraftLaunch;
+using Natsurainko.FluentCore.Extension.Windows.Model;
+using Natsurainko.FluentCore.Extension.Windows.Service;
+using MinecraftLaunch.Classes.Interfaces;
+using MinecraftLaunch.Components.Resolver;
+
+namespace ZMCL_neao.Views.Pages
+{
+    public partial class SettingsPage : INavigableView<SettingsViewModel>
+    {
+        public SettingsViewModel ViewModel { get; }
+        public static string javalist_ = new(javalist_);
+        public static string gamelist_ = new(gamelist_);
+        public SettingsPage(SettingsViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+            IGameResolver gameResolver = new GameResolver(".minecraft");
+            gamelist.ItemsSource = gameResolver.GetGameEntitys();
+            javalist.ItemsSource = JavaHelper.SearchJavaRuntime();
+            javalist_ = javalist.Text;
+            gamelist_ = gamelist.Text;
+        }
+    }
+}
